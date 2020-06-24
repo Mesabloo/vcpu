164a869ba9188fdb12127b8495ed59f924375ffd
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-24 17:39:51 +0200

    ✨ Implement the not gate

--- src/circuit/gate.rs
+++ src/circuit/gate.rs
@@ -12,3 +12,20 @@ pub struct Gate<'a> {
 // NOTE: When implementing gates, despite the fact that our bits are represented currently as bools,
 // we won't be using any of the boolean operators. In fact, we will code them all as if they never existed.
 
+use crate::units::bit::{OFF, ON};
+
+pub struct NotGate<'a>(Gate<'a>);
+impl<'a> Computeable<'a> for NotGate<'a> {
+    fn compute(self) {
+        // We do not use the "b" argument because "a == b"
+
+        // Truth table:
+        //
+        //   a  |  b  |  c
+        //  ON  | ON  | OFF
+        //  OFF | OFF | ON
+
+        self.0.c.set(if self.0.a.state() == ON { OFF } else { ON });
+    }
+}
+
