5e4140132b70281e7b83ada79755ef6d0e7bd434
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-24 17:37:54 +0200

    ♻️ Reformat code

--- src/units/bit.rs
+++ src/units/bit.rs
@@ -2,7 +2,7 @@
 ///
 /// We choose to represent a bit with a simple boolean and aliases for true and false (just for more convenient names).
 pub type Bit = bool;
-/// The "on" state of a bit. 
+/// The "on" state of a bit.
 pub const ON: Bit = true;
 /// The "off" state of a bit.
 pub const OFF: Bit = false;
