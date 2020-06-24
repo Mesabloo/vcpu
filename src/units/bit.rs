205b523a92209b0d98f25d5e43617c893d4709e3
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-24 15:48:52 +0200

    🚚 Move bits into its own module

---
+++ src/units/bit.rs
@@ -0,0 +1,8 @@
+/// A bit is anything that can be in only one of two states at once.
+///
+/// We choose to represent a bit with a simple boolean and aliases for true and false (just for more convenient names).
+type Bit = bool;
+/// The "on" state of a bit. 
+const ON: Bit = true;
+/// The "off" state of a bit.
+const OFF: Bit = false;
