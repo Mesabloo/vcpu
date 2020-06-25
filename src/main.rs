cb0b312961d29e0057bb8c2c3bbada26fc338cf2
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-25 12:51:47 +0200

    ✨ Include a main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use vcpu::circuit::{wire::Wire, gate::ANDGate};
+use vcpu::units::bit::{ON, OFF};
+use std::rc::Rc;
+use std::cell::RefCell;
+
 fn main() {
-    println!("Hello, world!");
+    let in1 = Rc::new(RefCell::new(Wire::default()));
+    let in2 = Rc::new(RefCell::new(Wire::default()));
+    let out = Rc::new(RefCell::new(Wire::default()));
+    in1.borrow_mut().set(OFF);
+    in2.borrow_mut().set(ON);
+    out.borrow_mut().set(OFF);
+
+    let and = ANDGate::new(in1, in2, out.clone());
+    and.run();
+
+    let out_bit = out.borrow().state();
+    assert_eq!(OFF, out_bit);
+
+    println!("Done!");
 }
