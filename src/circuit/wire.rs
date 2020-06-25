346e7ead6293f7e249f835408d9fb41cfeac0e38
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-25 13:53:50 +0200

    🎨 Create a newtype for better manipulation of wires

--- src/circuit/wire.rs
+++ src/circuit/wire.rs
@@ -1,4 +1,7 @@
 use crate::units::bit::Bit;
+use std::cell::RefCell;
+use std::ops::Deref;
+use std::rc::Rc;
 
 /// A wire is a basic unit containing only one bit at a time.
 pub struct Wire(pub Bit);
@@ -16,5 +19,22 @@ impl Wire {
 impl Default for Wire {
     fn default() -> Wire {
         Wire(Bit::default())
+    }
+}
+
+#[derive(Clone)]
+pub struct WireRef(Rc<RefCell<Wire>>);
+impl Default for WireRef {
+    fn default() -> WireRef {
+        WireRef(Rc::new(RefCell::new(Wire::default())))
+    }
+}
+impl Deref for WireRef {
+    type Target = <Rc<RefCell<Wire>> as Deref>::Target;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            WireRef(rc) => rc.deref(),
+        }
     }
 }
