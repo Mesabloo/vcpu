50d21c3d88d8186befaca3d7f7ce0c61f34317b2
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-25 15:02:49 +0200

    ✨ Implement memory gate

--- src/circuit/gate.rs
+++ src/circuit/gate.rs
@@ -80,3 +80,47 @@ impl NANDGate {
             });
     }
 }
+
+pub struct MemoryGate {
+    g1: NANDGate,
+    g2: NANDGate,
+    g3: NANDGate,
+    g4: NANDGate,
+}
+impl MemoryGate {
+    pub fn new(i: Wire, s: Wire, o: Wire) -> Self {
+        let a = Wire::default();
+        let b = Wire::default();
+        let c = Wire::default();
+
+        MemoryGate {
+            g1: NANDGate {
+                in1: i,
+                in2: s.clone(),
+                out: a.clone(),
+            },
+            g2: NANDGate {
+                in1: a.clone(),
+                in2: s,
+                out: b.clone(),
+            },
+            g3: NANDGate {
+                in1: a,
+                in2: c.clone(),
+                out: o.clone(),
+            },
+            g4: NANDGate {
+                in1: o,
+                in2: b,
+                out: c,
+            },
+        }
+    }
+
+    pub fn run(self) {
+        self.g1.run();
+        self.g2.run();
+        self.g3.run();
+        self.g4.run();
+    }
+}
