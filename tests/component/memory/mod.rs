dc47359069bb9a9822db6ff8da3a89a68dc7d4f1
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-29 18:53:49 +0200

    ✅ Test memory byte holder

--- tests/component/memory/mod.rs
+++ tests/component/memory/mod.rs
@@ -2,7 +2,7 @@
 
 use vcpu::circuit::wire::{Bus, Wire};
 use vcpu::units::bit::{OFF, ON};
-use vcpu::component::memory::{bit::*};
+use vcpu::component::memory::{bit::*, byte::*};
 
 #[test]
 fn memory_bit_no_set() {
@@ -46,4 +46,55 @@ fn memory_bit_set_unset() {
     mem_bit.run();
 
     assert_eq!(out.state(), ON);
+}
+
+#[test]
+fn memory_byte_no_set() {
+    let b_in = Bus::default();
+    let b_out = Bus::default();
+    let set = Wire::default();
+    let mem_byte = MemoryByte::new(b_in.clone(), set.clone(), b_out.clone());
+
+    b_in[2].set(ON);
+    b_in[5].set(ON);
+    mem_byte.run();
+
+    assert_eq!(b_out[2].state(), OFF);
+    assert_eq!(b_out[5].state(), OFF);
+}
+
+#[test]
+fn memory_byte_set() {
+    let b_in = Bus::default();
+    let b_out = Bus::default();
+    let set = Wire::default();
+    let mem_byte = MemoryByte::new(b_in.clone(), set.clone(), b_out.clone());
+
+    b_in[2].set(ON);
+    b_in[5].set(ON);
+    set.set(ON);
+    mem_byte.run();
+
+    assert_eq!(b_out[2].state(), ON);
+    assert_eq!(b_out[5].state(), ON);
+}
+
+#[test]
+fn memory_byte_set_unset() {
+    let b_in = Bus::default();
+    let b_out = Bus::default();
+    let set = Wire::default();
+    let mem_byte = MemoryByte::new(b_in.clone(), set.clone(), b_out.clone());
+
+    b_in[2].set(ON);
+    b_in[5].set(ON);
+    set.set(ON);
+    mem_byte.run();
+    b_in[2].set(OFF);
+    b_in[5].set(OFF);
+    set.set(OFF);
+    mem_byte.run();
+
+    assert_eq!(b_out[2].state(), ON);
+    assert_eq!(b_out[5].state(), ON);
 }
