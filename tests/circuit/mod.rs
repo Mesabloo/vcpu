022e977d56aeecae2f4ac01c110fe126eb69c848
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-29 16:39:26 +0200

    ✅ Test NAND gate

--- tests/circuit/mod.rs
+++ tests/circuit/mod.rs
@@ -77,3 +77,59 @@ fn notgate_input_off() {
 
     assert_eq!(out.state(), ON);
 }
+
+#[test]
+fn nandgate_input_off_off() {
+    let in1 = Wire::default();
+    let in2 = Wire::default();
+    let out = Wire::default();
+    let gate = NANDGate::new(in1.clone(), in2.clone(), out.clone());
+
+    in1.set(OFF);
+    in2.set(OFF);
+    gate.run();
+
+    assert_eq!(out.state(), ON);
+}
+
+#[test]
+fn nandgate_input_off_on() {
+    let in1 = Wire::default();
+    let in2 = Wire::default();
+    let out = Wire::default();
+    let gate = NANDGate::new(in1.clone(), in2.clone(), out.clone());
+
+    in1.set(OFF);
+    in2.set(ON);
+    gate.run();
+
+    assert_eq!(out.state(), ON);
+}
+
+#[test]
+fn nandgate_input_on_off() {
+    let in1 = Wire::default();
+    let in2 = Wire::default();
+    let out = Wire::default();
+    let gate = NANDGate::new(in1.clone(), in2.clone(), out.clone());
+
+    in1.set(ON);
+    in2.set(OFF);
+    gate.run();
+
+    assert_eq!(out.state(), ON);
+}
+
+#[test]
+fn nandgate_input_on_on() {
+    let in1 = Wire::default();
+    let in2 = Wire::default();
+    let out = Wire::default();
+    let gate = NANDGate::new(in1.clone(), in2.clone(), out.clone());
+
+    in1.set(ON);
+    in2.set(ON);
+    gate.run();
+
+    assert_eq!(out.state(), OFF);
+}
