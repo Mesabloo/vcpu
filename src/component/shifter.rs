025b0edb34a3d19085766af292b3ad797783b944
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-29 11:20:36 +0200

    ✨ Implement left and right shifters

---
+++ src/component/shifter.rs
@@ -0,0 +1,52 @@
+use crate::circuit::wire::{Bus, Wire};
+use crate::common::BUS_WIDTH;
+
+pub struct LeftShifter {
+    bus1: Bus,
+    bus2: Bus,
+    shift_in: Wire,
+    shift_out: Wire,
+}
+impl LeftShifter {
+    pub fn new(i: Bus, o: Bus, shift_in: Wire, shift_out: Wire) -> Self {
+        LeftShifter {
+            bus1: i,
+            bus2: o,
+            shift_in,
+            shift_out,
+        }
+    }
+
+    pub fn run(&self) {
+        for idx in 1..BUS_WIDTH - 1 {
+            self.bus2[idx - 1].set(self.bus1[idx].state());
+        }
+        self.bus2[BUS_WIDTH - 1].set(self.shift_in.state());
+        self.shift_out.set(self.bus1[0].state());
+    }
+}
+
+pub struct RightShifter {
+    bus1: Bus,
+    bus2: Bus,
+    shift_in: Wire,
+    shift_out: Wire,
+}
+impl RightShifter {
+    pub fn new(i: Bus, o: Bus, shift_in: Wire, shift_out: Wire) -> Self {
+        RightShifter {
+            bus1: i,
+            bus2: o,
+            shift_in,
+            shift_out,
+        }
+    }
+
+    pub fn run(&self) {
+        for idx in 1..BUS_WIDTH {
+            self.bus2[idx].set(self.bus1[idx - 1].state());
+        }
+        self.bus2[0].set(self.shift_in.state());
+        self.shift_out.set(self.bus1[BUS_WIDTH - 1].state());
+    }
+}
