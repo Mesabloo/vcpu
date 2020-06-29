0142cee47c01f3204b29675d8e9223614e731ef1
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-29 12:11:10 +0200

    ✅ Test enabler

--- src/component/enabler.rs
+++ src/component/enabler.rs
@@ -1,5 +1,9 @@
 use crate::circuit::gate::ANDGate;
 use crate::circuit::wire::{Bus, Wire};
+#[cfg(test)]
+use crate::common::BUS_WIDTH;
+#[cfg(test)]
+use crate::units::bit::{OFF, ON};
 
 pub struct Enabler {
     gates: Vec<ANDGate>, // must be of a length of "BUS_WIDTH"
@@ -18,4 +22,35 @@ impl Enabler {
     pub fn run(&self) {
         self.gates.iter().for_each(|g| g.run());
     }
+}
+
+#[cfg(test)]
+#[test]
+fn test_enabler() {
+    let b1 = Bus::default();
+    let enable = Wire::default();
+
+    b1[2].set(ON);
+    b1[3].set(ON);
+    b1[4].set(ON);
+
+    let enabler = Enabler::new(b1.clone(), enable.clone(), b1.clone());
+    enabler.run();
+
+    assert_eq!(
+        b1.iter().map(|w| w.state()).collect::<Vec<_>>(),
+        vec![OFF; BUS_WIDTH]
+    );
+
+    enable.set(ON);
+    b1[2].set(ON);
+    b1[3].set(ON);
+    b1[4].set(ON);
+
+    enabler.run();
+
+    assert_eq!(
+        b1.iter().map(|w| w.state()).collect::<Vec<_>>(),
+        vec![OFF, OFF, ON, ON, ON, OFF, OFF, OFF]
+    );
 }
