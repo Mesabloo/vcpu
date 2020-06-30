f36634f10dca6274b8c2c47d49f3bfba9d5afe87
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-30 11:25:46 +0200

    ♻️ Use 4x16 decoder in RAM

--- src/component/memory/ram/mod.rs
+++ src/component/memory/ram/mod.rs
@@ -2,8 +2,9 @@ pub mod cell;
 
 use crate::circuit::wire::{Bus, Wire};
 use crate::common::BUS_WIDTH;
-use crate::component::decoder::Decoder;
+use crate::component::decoder::Decoder4X16;
 use crate::component::memory::register::Register;
+use crate::units::bit::ON;
 use cell::Cell;
 
 use std::iter::repeat_with;
@@ -13,14 +14,16 @@ const DECODER_OUT_NB: usize = 2usize.pow(DECODER_IN_NB as u32);
 
 pub struct RAM {
     mar: Register,
-    dh: Decoder<DECODER_IN_NB, DECODER_OUT_NB>,
-    dv: Decoder<DECODER_IN_NB, DECODER_OUT_NB>,
+    dh: Decoder4X16,
+    dv: Decoder4X16,
     cells: Vec<Cell>,
 }
 impl RAM {
     pub fn new(bus1: Bus, bus2: Bus, sa: Wire, s: Wire, e: Wire) -> Self {
         let mut reg_to_dec_s: Vec<_> = repeat_with(Wire::default).take(BUS_WIDTH).collect();
-        let r = Register::new(bus1, sa.clone(), sa, Bus::from(reg_to_dec_s.clone()));
+        let always_on = Wire::default();
+        always_on.set(ON);
+        let r = Register::new(bus1, sa.clone(), always_on, Bus::from(reg_to_dec_s.clone()));
 
         let mut dec_to_bus_s: Vec<_> = repeat_with(Wire::default)
             .take(2 * DECODER_OUT_NB)
@@ -32,8 +35,8 @@ impl RAM {
         let dh_to_bus = dec_to_bus_s.split_off(DECODER_OUT_NB);
         let dv_to_bus = dec_to_bus_s;
 
-        let dh = Decoder::new(reg_to_dh, dh_to_bus.clone());
-        let dv = Decoder::new(reg_to_dv, dv_to_bus.clone());
+        let dh = Decoder4X16::new(reg_to_dh, dh_to_bus.clone());
+        let dv = Decoder4X16::new(reg_to_dv, dv_to_bus.clone());
 
         let cells = (0..2usize.pow(DECODER_OUT_NB as u32))
             .map(|i| {
