f74ad2c2549600fb3a65caa462c47a3c19b3bff1
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-30 10:34:28 +0200

    🎨 Replace AND gates with multi AND gates

--- src/component/decoder.rs
+++ src/component/decoder.rs
@@ -1,17 +1,16 @@
 use crate::circuit::gate::{MultiANDGate, NOTGate};
 use crate::circuit::wire::Wire;
 
-use std::iter::{repeat, repeat_with};
-use std::ops::{Generator, GeneratorState};
-use std::pin::Pin;
+use itertools::Itertools;
+use std::iter::repeat_with;
 
 pub struct Decoder2X4 {
     not_a: NOTGate,
     not_b: NOTGate,
-    not_a_not_b: ANDGate,
-    not_a_b: ANDGate,
-    a_not_b: ANDGate,
-    a_b: ANDGate,
+    not_a_not_b: MultiANDGate,
+    not_a_b: MultiANDGate,
+    a_not_b: MultiANDGate,
+    a_b: MultiANDGate,
 }
 impl Decoder2X4 {
     pub fn new(
@@ -28,10 +27,10 @@ impl Decoder2X4 {
         Decoder2X4 {
             not_a: NOTGate::new(in_a.clone(), tmp_na.clone()),
             not_b: NOTGate::new(in_b.clone(), tmp_nb.clone()),
-            not_a_not_b: ANDGate::new(tmp_na.clone(), tmp_nb.clone(), out_na_nb),
-            not_a_b: ANDGate::new(tmp_na, in_b.clone(), out_na_b),
-            a_not_b: ANDGate::new(in_a.clone(), tmp_nb, out_a_nb),
-            a_b: ANDGate::new(in_a, in_b, out_a_b),
+            not_a_not_b: MultiANDGate::new(vec![tmp_na.clone(), tmp_nb.clone()], out_na_nb),
+            not_a_b: MultiANDGate::new(vec![tmp_na, in_b.clone()], out_na_b),
+            a_not_b: MultiANDGate::new(vec![in_a.clone(), tmp_nb], out_a_nb),
+            a_b: MultiANDGate::new(vec![in_a, in_b], out_a_b),
         }
     }
 
