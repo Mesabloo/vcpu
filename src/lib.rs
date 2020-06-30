8f9e00b7c4abba9bffa10e039f30aa9fc67d212a
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-30 10:34:49 +0200

    🔥 Remove unused features

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-#![allow(incomplete_features)] // for const_generics
-#![feature(const_generics)]
-#![feature(const_int_pow)]
-#![feature(generators, generator_trait)]
 #![feature(iterator_fold_self)]
 
 pub mod circuit;
