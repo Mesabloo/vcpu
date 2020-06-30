a5758bf336c34bc5804944998fe250ba3eb5d52c
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-06-30 11:25:08 +0200

    ✨ Implement 4x16 decoder

--- src/component/decoder.rs
+++ src/component/decoder.rs
@@ -1,6 +1,8 @@
 use crate::circuit::gate::{MultiANDGate, NOTGate};
 use crate::circuit::wire::Wire;
 
+use std::iter::repeat_with;
+
 pub struct Decoder2X4 {
     not_a: NOTGate,
     not_b: NOTGate,
@@ -39,5 +41,182 @@ impl Decoder2X4 {
         self.not_a_b.run();
         self.a_not_b.run();
         self.a_b.run();
+    }
+}
+
+pub struct Decoder4X16 {
+    nots: Vec<NOTGate>,
+    ands: Vec<MultiANDGate>,
+}
+impl Decoder4X16 {
+    pub fn new(ins: Vec<Wire>, outs: Vec<Wire>) -> Self {
+        assert_eq!(ins.len(), 4);
+        assert_eq!(outs.len(), 16 /* 2usize.pow(4) */);
+
+        let tmps: Vec<_> = repeat_with(Wire::default).take(ins.len()).collect();
+
+        let nots: Vec<_> = ins
+            .clone()
+            .into_iter()
+            .zip(tmps.clone().into_iter())
+            .map(|(i, o)| NOTGate::new(i, o))
+            .collect();
+
+        // Let's write all the different combinations by hand:
+        // outs[0]:  0/0/0/0
+        // outs[1]:  0/0/0/1
+        // outs[2]:  0/0/1/0
+        // outs[3]:  0/0/1/1
+        // outs[4]:  0/1/0/0
+        // outs[5]:  0/1/0/1
+        // outs[6]:  0/1/1/0
+        // outs[7]:  0/1/1/1
+        // outs[8]:  1/0/0/0
+        // outs[9]:  1/0/0/1
+        // outs[10]: 1/0/1/0
+        // outs[11]: 1/0/1/1
+        // outs[12]: 1/1/0/0
+        // outs[13]: 1/1/0/1
+        // outs[14]: 1/1/1/0
+        // outs[15]: 1/1/1/1
+
+        let ands = vec![
+            MultiANDGate::new(tmps.clone(), outs[0].clone()),
+            MultiANDGate::new(
+                vec![
+                    tmps[0].clone(),
+                    tmps[1].clone(),
+                    tmps[2].clone(),
+                    ins[3].clone(),
+                ],
+                outs[1].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    tmps[0].clone(),
+                    tmps[1].clone(),
+                    ins[2].clone(),
+                    tmps[3].clone(),
+                ],
+                outs[2].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    tmps[0].clone(),
+                    tmps[1].clone(),
+                    ins[2].clone(),
+                    ins[3].clone(),
+                ],
+                outs[3].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    tmps[0].clone(),
+                    ins[1].clone(),
+                    tmps[2].clone(),
+                    tmps[3].clone(),
+                ],
+                outs[4].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    tmps[0].clone(),
+                    ins[1].clone(),
+                    tmps[2].clone(),
+                    ins[3].clone(),
+                ],
+                outs[5].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    tmps[0].clone(),
+                    ins[1].clone(),
+                    ins[2].clone(),
+                    tmps[3].clone(),
+                ],
+                outs[6].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    tmps[0].clone(),
+                    ins[1].clone(),
+                    ins[2].clone(),
+                    ins[3].clone(),
+                ],
+                outs[7].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    ins[0].clone(),
+                    tmps[1].clone(),
+                    tmps[2].clone(),
+                    tmps[3].clone(),
+                ],
+                outs[8].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    ins[0].clone(),
+                    tmps[1].clone(),
+                    tmps[2].clone(),
+                    ins[3].clone(),
+                ],
+                outs[9].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    ins[0].clone(),
+                    tmps[1].clone(),
+                    ins[2].clone(),
+                    tmps[3].clone(),
+                ],
+                outs[10].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    ins[0].clone(),
+                    tmps[1].clone(),
+                    ins[2].clone(),
+                    ins[3].clone(),
+                ],
+                outs[11].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    ins[0].clone(),
+                    ins[1].clone(),
+                    tmps[2].clone(),
+                    tmps[3].clone(),
+                ],
+                outs[12].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    ins[0].clone(),
+                    ins[1].clone(),
+                    tmps[2].clone(),
+                    ins[3].clone(),
+                ],
+                outs[13].clone(),
+            ),
+            MultiANDGate::new(
+                vec![
+                    ins[0].clone(),
+                    ins[1].clone(),
+                    ins[2].clone(),
+                    tmps[3].clone(),
+                ],
+                outs[14].clone(),
+            ),
+            MultiANDGate::new(ins.clone(), outs[15].clone()),
+        ];
+        // We will try to find a generic way of doing this based on const generics. For now it doesn't really matter.
+        // The problem right now is that the order of combinations matters.
+
+        Decoder4X16 { nots, ands }
+    }
+    pub fn run(&self) {
+        self.nots.iter().for_each(|g| g.run());
+        self.ands.iter().for_each(|g| g.run());
     }
 }
